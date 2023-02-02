519155758f78838b351beea15efad72c95c9df36
PKG-7 <pavel.k.group@gmail.com>
2023-02-02 21:51:09 +0300

    start styling

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,6 +2,7 @@ import { Item } from '@/pages'
 import Image from 'next/image'
 import React from 'react'
 import style from './Card.module.scss'
+import favorite from '../../images/icons/favorite.svg'
 
 interface Props {
     item: Item
@@ -9,9 +10,24 @@ interface Props {
 
 export default function Card({ item }: Props) {
     return (
-        <div className={style.cover}>
-            <div>{item.title}</div>
-            <Image src={item.image} width={220} height={220} alt={item.title} />
+        <div className={style.card}>
+            <div className={style.container}>
+                <div className={style.container}>
+                    <Image src={item.image} width={220} height={220} alt={item.title} />
+                </div>
+
+                <div>
+                    <div className={style['category']}>{item.category}</div>
+                    <div className={style['title']}>{item.title}</div>
+                    <div className={style['price']}>
+                        {item.price} ₽ <span className={style['price_sign']}>/шт.</span>
+                    </div>
+                    <div className={style['button_container']}>
+                        <button className={style['cart_button']}>В корзине</button>
+                        <Image src={favorite} alt='' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
