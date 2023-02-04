c0c53022ee621a7bef1adf29bd6f1684d3672e83
PKG-7 <pavel.k.group@gmail.com>
2023-02-04 22:39:09 +0300

    fix headewr

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,28 +8,26 @@ export interface IHeader {
 }
 
 export function Header({ shoppingCart, favorites, items }: IHeader) {
-    if (shoppingCart.length > 0 || favorites.length > 0)
-        return (
-            <div className={styles.container}>
-                <div className={styles.cart}>
-                    In cart:{' '}
-                    {shoppingCart.map((cartItem) => (
-                        <div className={styles.card} key={cartItem.id}>
-                            <p>Item ID: {cartItem.id}</p>
-                            <p>Amount: {cartItem.amount}</p>
-                        </div>
-                    ))}
-                </div>
-
-                <div className={styles.favorites}>
-                    Favorites:
-                    {favorites.map((favoriteItem) => (
-                        <div className={styles.card} key={favoriteItem}>
-                            {favoriteItem}
-                        </div>
-                    ))}
-                </div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.cart}>
+                In cart:{' '}
+                {shoppingCart.map((cartItem) => (
+                    <div className={styles.card} key={cartItem.id}>
+                        <p>Item ID: {cartItem.id}</p>
+                        <p>Amount: {cartItem.amount}</p>
+                    </div>
+                ))}
             </div>
-        )
-    return null
+
+            <div className={styles.favorites}>
+                Favorites:
+                {favorites.map((favoriteItem) => (
+                    <div className={styles.card} key={favoriteItem}>
+                        {favoriteItem}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
